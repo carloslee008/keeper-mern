607036a1b2fe0ef5faaa264f8cef9e7341c15575
carloslee008 <59149514+carloslee008@users.noreply.github.com>
2023-02-15 21:52:51 -0500

    first commit

---
+++ backend/models/note.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+  title: {type:String},
+  content: {type:String}
+});
+
+
+const Note = new mongoose.model("Note", noteSchema);
+
+module.exports = Note;
